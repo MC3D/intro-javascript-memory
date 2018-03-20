4dc87fc60a75a274b5b5f0d496a27f351bbaf7fd
MC3D <m.chapman.home@gmail.com>
2018-03-20 17:28:03 -0400

    adds basic game functionality

--- main.js
+++ main.js
@@ -2,139 +2,175 @@
 
   'use strict';
 
-  
+  // The JavaScript Array object is a global object that is used in the construction of arrays; which are high-level, list-like objects
 
-  // BUILD HTML
+  let cards = [
+    {
+      id: 1,
+      name: 'card-one',
+      img: './images/image1.jpg'
+    }, {
+      id: 2,
+      name: 'card-two',
+      img: './images/image2.jpg'
+    }, {
+      id: 3,
+      name: 'card-three',
+      img: './images/image3.jpg'
+    }, {
+      id: 4,
+      name: 'card-four',
+      img: './images/image4.jpg'
+    }, {
+      id: 5,
+      name: 'card-five',
+      img: './images/image5.jpg'
+    }, {
+      id: 6,
+      name: 'card-six',
+      img: './images/image6.jpg'
+    }, {
+      id: 7,
+      name: 'card-seven',
+      img: './images/image7.jpg'
+    }, {
+      id: 8,
+      name: 'card-eight',
+      img: './images/image8.jpg'
+    }, {
+      id: 9,
+      name: 'card-nine',
+      img: './images/image9.jpg'
+    }, {
+      id: 10,
+      name: 'card-ten',
+      img: './images/image10.jpg'
+    }, {
+      id: 11,
+      name: 'card-eleven',
+      img: './images/image11.jpg'
+    }, {
+      id: 12,
+      name: 'card-twelve',
+      img: './images/image12.jpg'
+    }
+  ]
 
-  // var source   = document.getElementById("card-template").innerHTML;
-  // var template = Handlebars.compile(source);
-  //
-  // for (let i = 1; i <= 12; i++) {
-  //   let context = {id: i, image: `./images/image${i}.jpg`};
-  //   var html = template(context);
-  //   // console.log(html);
-  //   $('.cards').append(html);
-  // }
+  // Classes are in fact "special functions"
+  class Game {
+    // The constructor method is a special method for creating and initializing an object created within a class
+    constructor() {
+      // double the cards (24 in total); using the jQuery library
+      this.cardsArray = $.merge(cards, cards);
+      // console.log(this.cardsArray);
+      // shuffle the cards
+      this.shuffleCards(this.cardsArray);
+      this.setup();
+    }
 
-  // cards.forEach((card) => {
-  //   let context = card;
-  //   let html = template(context);
-  // });
+    setup() {
+      this.guess = null;
+      this.matches = 0;
 
-  // console.log(cards);
+      this.buildHTML(this.cardsArray);
 
-  // var Memory = {
-  //
-  //   init: function(cards) {
-  //     this.$game = $(".game");
-  //     this.$modal = $(".modal");
-  //     this.$overlay = $(".modal-overlay");
-  //     this.$restartButton = $("button.restart");
-  //     this.cardsArray = $.merge(cards, cards);
-  //     this.shuffleCards(this.cardsArray);
-  //     this.setup();
-  //   },
-  //
-  //   shuffleCards: function(cardsArray) {
-  //     this.$cards = $(this.shuffle(this.cardsArray));
-  //   },
-  //
-  //   setup: function() {
-  //     this.html = this.buildHTML();
-  //     this.$game.html(this.html);
-  //     this.$memoryCards = $(".card");
-  //     this.paused = false;
-  //     this.guess = null;
-  //     this.binding();
-  //   },
-  //
-  //   binding: function() {
-  //     this.$memoryCards.on("click", this.cardClicked);
-  //     this.$restartButton.on("click", $.proxy(this.reset, this));
-  //   },
-  //    kinda messy but hey
-  //   cardClicked: function() {
-  //     var _ = Memory;
-  //     var $card = $(this);
-  //     if (!_.paused && !$card.find(".inside").hasClass("matched") && !$card.find(".inside").hasClass("picked")) {
-  //       $card.find(".inside").addClass("picked");
-  //       if (!_.guess) {
-  //         _.guess = $(this).attr("data-id");
-  //       } else if (_.guess == $(this).attr("data-id") && !$(this).hasClass("picked")) {
-  //         $(".picked").addClass("matched");
-  //         _.guess = null;
-  //       } else {
-  //         _.guess = null;
-  //         _.paused = true;
-  //         setTimeout(function() {
-  //           $(".picked").removeClass("picked");
-  //           Memory.paused = false;
-  //         }, 600);
-  //       }
-  //       if ($(".matched").length == $(".card").length) {
-  //         _.win();
-  //       }
-  //     }
-  //   },
-  //
-  //   win: function() {
-  //     this.paused = true;
-  //     setTimeout(function() {
-  //       Memory.showModal();
-  //       Memory.$game.fadeOut();
-  //     }, 1000);
-  //   },
-  //
-  //   showModal: function() {
-  //     this.$overlay.show();
-  //     this.$modal.fadeIn("slow");
-  //   },
-  //
-  //   hideModal: function() {
-  //     this.$overlay.hide();
-  //     this.$modal.hide();
-  //   },
-  //
-  //   reset: function() {
-  //     this.hideModal();
-  //     this.shuffleCards(this.cardsArray);
-  //     this.setup();
-  //     this.$game.show("slow");
-  //   },
-  //
-  //    Fisher--Yates Algorithm -- https://bost.ocks.org/mike/shuffle/
-  //   shuffle: function(array) {
-  //     var counter = array.length,
-  //       temp,
-  //       index;
-  //      While there are elements in the array
-  //     while (counter > 0) {
-  //        Pick a random index
-  //       index = Math.floor(Math.random() * counter);
-  //        Decrease counter by 1
-  //       counter--;
-  //        And swap the last element with it
-  //       temp = array[counter];
-  //       array[counter] = array[index];
-  //       array[index] = temp;
-  //     }
-  //     return array;
-  //   },
-  //
-  //   buildHTML: function() {
-  //     var frag = '';
-  //     this.$cards.each(function(k, v) {
-  //       frag += '<div class="card" data-id="' + v.id + '"><div class="inside">\
-  			// <div class="front"><img src="' + v.img + '"\
-  			// alt="' + v.name + '" /></div>\
-  			// <div class="back"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/codepen-logo.png"\
-  			// alt="Codepen" /></div></div>\
-  			// </div>';
-  //     });
-  //     return frag;
-  //   }
-  // };
-  //
-  // Memory.init(cards);
+      // $(".card") returns jQuery wrapped DOM elements - allows us to call jQuery methods
+      this.$memoryCards = $(".card");
+      // console.log('cards', this.$memoryCards);
+
+      this.$memoryCards.on("click", this.cardClicked.bind(this));
+    }
+
+    reset() {
+      //// ADD RESET METHOD
+    }
+
+    shuffleCards(array) {
+      let card,
+        index;
+
+      for (let i = 0; i < array.length; i++) {
+        // Pick a random index
+        index = Math.floor(Math.random() * i);
+
+        card = array[i]; // pull card out of the array we passed in
+        array[i] = array[index];
+        array[index] = card;
+
+        // let's walk through this using 0 for the value of i
+
+        // card = array[i];  card is an object
+        // card = {
+        //   id: 1,
+        //   name: 'card-one',
+        //   img: './images/image1.jpg'}
+        // };
+
+        // array[i] = array[index];
+        // the first card in the array is replaced with a random card from the array
+
+        // put the first card back into the array at the random position - so we don't lose that card
+        // array[index] = card;
+      }
+      // console.log(array);
+      return array;
+    }
+
+    buildHTML(array) {
+      // BUILD HTML
+
+      // handlebars allows you to build templates
+      // you can deliver a template to the browser by including it in a <script> tag
+      // takes any HTML and Handlebars expression and compiles them to a JavaScript function
+      // derived function takes one parameter, an object — your data — and it returns an HTML string with the object properties’ values inserted into the HTML
+
+      var source = document.getElementById("card-template").innerHTML;
+      var template = Handlebars.compile(source); // returns a function
+
+      // console.log('template', template);
+
+      var context = {
+        cards
+      };
+      var html = template(context);
+      $('.cards').append(html);
+    }
+
+    cardClicked(event) {
+      // events bubbles up
+      // console.log('event target', event.target);
+      // console.log('event currentTarget', event.currentTarget);
+      let $card = $(event.currentTarget);
+      let id = $card.find(".front").attr("data-id");
+      $card.toggleClass("selected");
+
+      if(this.guess === null){
+        this.guess = id;
+      } else if (this.guess === id) {
+        // add matched class to both cards
+        let $match = $(".selected").not($(".matched"));
+        $match.toggleClass("matched");
+        // remove click event
+        $match.off();
+        this.guess = null;
+
+        this.matches++;
+
+        /// ADD MODAL TO POPUP /////////////////////////////////////////////////
+        this.matches === 12 ? alert('you win') : null;
+      } else {
+        let $mismatch = $(".selected").not($(".matched"));
+        // $mismatch.toggleClass("selected");
+        setTimeout(()=>{
+          $mismatch.toggleClass("selected");
+        }, 1000)
+        this.guess = null;
+      }
+    }
+  }
+
+  let game = new Game();
+
+  // console.log('game', game);
 
 })();
